b27530f7a89d91097bef9a66730fae667fc4d32c
Andrew Njoo <adnjoo@gmail.com>
2021-09-09 21:39:25 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-three-fiber-image
+
+## [link](adnjoo.github.io/react-three-fiber-image)
---
+++ build/precache-manifest.6f3fd69348707d20cb8dfa9459ae9a35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d0a09fb8a36c2fef5ab5cf5d4a78b9",
+    "url": "/react-three-fiber-image/index.html"
+  },
+  {
+    "revision": "a456002df5887da2781d",
+    "url": "/react-three-fiber-image/static/css/main.e2d76b3d.chunk.css"
+  },
+  {
+    "revision": "36c66c56e0c4ac6eb30b",
+    "url": "/react-three-fiber-image/static/js/2.17f9faab.chunk.js"
+  },
+  {
+    "revision": "a456002df5887da2781d",
+    "url": "/react-three-fiber-image/static/js/main.50312454.chunk.js"
+  },
+  {
+    "revision": "8d20f3354b6c2b282995",
+    "url": "/react-three-fiber-image/static/js/runtime~main.24c1d5c3.js"
+  }
+]);
