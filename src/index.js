a7f09ca28ef8778917a121b16f0dfbd917f9f5cd
Andrew Njoo <adnjoo@gmail.com>
2021-09-10 09:00:49 -0700

    upload img

--- src/index.js
+++ src/index.js
@@ -1,35 +1,50 @@
 import { render } from 'react-dom'
 import { TextureLoader } from 'three'
 import { useSpring, animated } from 'react-spring/three'
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import { Canvas } from 'react-three-fiber'
 import { HoverImageShader } from './shaders/HoverImageShader'
 import './styles.css'
 
-const image = 'https://raw.githubusercontent.com/adnjoo/artExplorer/main/assets/mona2.png'
+const images = [
+  'https://raw.githubusercontent.com/adnjoo/artExplorer/main/assets/mona2.png',
+  // 'https://static.frieze.com/files/styles/frieze_teaser/public/article/thumbnail/editorial-articles-richardserra1-0.png'
+  'https://d5wt70d4gnm1t.cloudfront.net/media/a-s/artworks/takashi-murakami/71970-839162486768/takashi-murakami-a-little-flower-painting-yellow-white-and-purple-f-800x800.jpg'
+]
+let counter = 0;
 
-function Image({ url, ...props }) {
-  const [texture] = useMemo(() => {
-    const loader = new TextureLoader()
-    return [loader.load(url)]
-  }, [url])
 
-  const changeImage = () => {
-    console.log('changeImage')
-  }
-
-  return (
-    <animated.mesh
-     {...props}
-     onClick={(e)=>changeImage()}
-     >
-      <planeBufferGeometry attach="geometry" args={[5, 7]} />
-      <animated.shaderMaterial attach="material" transparent args={[HoverImageShader]} uniforms-texture-value={texture} />
-    </animated.mesh>
-  )
-}
 
 function App() {
+  function Image({ url, ...props }) {
+    
+    const [texture] = useMemo(() => {
+      const loader = new TextureLoader()
+      return [loader.load(url)]
+    }, [url])
+  
+    const changeImage = () => {
+      console.log('test')
+      if(counter+1==images.length){
+        counter=0
+        return
+      }
+      counter++
+      setImage(images[counter])
+    }
+  
+    return (
+      <animated.mesh
+       {...props}
+       onClick={(e)=>changeImage()}
+       >
+        <planeBufferGeometry attach="geometry" args={[5, 7]} />
+        <animated.shaderMaterial attach="material" transparent args={[HoverImageShader]} uniforms-texture-value={texture} />
+      </animated.mesh>
+    )
+  }
+  let [image, setImage] = useState('https://raw.githubusercontent.com/adnjoo/artExplorer/main/assets/mona2.png')
+  
   const [props, set] = useSpring(() => ({
     pos: [0, 0, 0],
     scale: [1, 1, 1],
